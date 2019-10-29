0862aee3a96ad45c761e42252909e40fff537ce2
Manuel Ferreira Junior <mf032354@hotmail.com>
2019-10-29 19:45:09 -0300

    atualiza responseDB.py

--- responseDB.py
+++ responseDB.py
@@ -55,8 +55,9 @@ responses = {
         'stats101-2019-26-10.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-26-10.csv'),
         'stats101-2019-27-10.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-27-10.csv'),
         'stats101-2019-28-10.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-28-10.csv'),
-        'stats101-2019-29-10.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-29-10.csv')
-        'stats101-2019-30-10.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-30-10.csv')
+        'stats101-2019-29-10.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-29-10.csv'),
+        'stats101-2019-30-10.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-30-10.csv'),
+        'stats101-2019-31-10.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-31-10.csv')
         },
     'scale': {
 
@@ -69,7 +70,7 @@ responses_type = list(responses.keys())
 
 print(responses_type)
 
-db = responses['open_ended']['stats101-2019-30-10.csv'].read()
+db = responses['open_ended']['stats101-2019-31-10.csv'].read()
 
 print(db.normalize())
 #
