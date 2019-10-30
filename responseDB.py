c56a233904c1f31f09216ef4b26d157c6bb7a64e
Manuel Ferreira Junior <mf032354@hotmail.com>
2019-10-29 21:29:55 -0300

    atualiza responseDB.py

--- responseDB.py
+++ responseDB.py
@@ -58,7 +58,8 @@ responses = {
         'stats101-2019-29-10.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-29-10.csv'),
         'stats101-2019-30-10.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-30-10.csv'),
         'stats101-2019-31-10.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-31-10.csv'),
-        'stats101-2019-01-11.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-01-11.csv')
+        'stats101-2019-01-11.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-01-11.csv'),
+        'stats101-2019-03-11.csv': Dataset('https://raw.githubusercontent.com/responsedb/datasets/master/open-ended/error-counts/stats101-2019-03-11.csv')
         },
     'scale': {
 
@@ -71,7 +72,7 @@ responses_type = list(responses.keys())
 
 print(responses_type)
 
-db = responses['open_ended']['stats101-2019-01-11.csv'].read()
+db = responses['open_ended']['stats101-2019-03-11.csv'].read()
 
 print(db.normalize())
 #
